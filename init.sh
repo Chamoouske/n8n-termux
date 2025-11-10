a12eae6a4d4c492cae6eb44c12bfcf3928348f90
Ajax Lima <56363968+Chamoouske@users.noreply.github.com>
2025-11-10 23:12:36 +0000

    feat: arquivo com script para preparar o ambiente

---
+++ init.sh
@@ -0,0 +1,51 @@
+pkg install root-repo
+
+pkg update
+
+#Instala o Python 3
+
+pkg install python3
+
+#Instalar o nodeJS 16x
+
+pkg install nodejs-lts
+
+#Instalar o SQL Lite3 compilando ele do Source
+
+pkg install clang libsqlite pkg-config python make
+
+npm install sqlite3 --build-from-source --sqlite=/data/data/com.termux/files/usr/bin/sqlite3 -g
+
+#Instalar o n8n indicando o caminho do sqllite
+
+npm install n8n -g --sqlite=/data/data/com.termux/files/usr/bin/sqlite3
+
+Instalar o pm2
+
+npm install pm2 -g
+
+#rodar o pm2
+
+#opção sem tunnel para webhooks
+
+pm2 start n8n 
+
+#opção com tunnel para webhooks (não utilize em produção)
+
+pm2 start n8n —tunnel
+
+#salvar o estado do pm2
+
+pm2 save
+
+#Colocando o n8n no Startup do pm2 start (inicio automático)
+
+#ver status
+
+pm2 status
+
+#ver o ip do teu termux
+
+pkg install net-tools
+
+ifconfig
